e6788ac38639959845990d03e2da8020e32f1db4
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2022-05-06 12:41:14 +0000

    [add] io module

---
+++ src/io.rs
@@ -0,0 +1,23 @@
+#[allow(dead_code)]
+fn file_exist(_file: &str) -> bool {
+    false
+}
+
+#[test]
+fn test_file_exist(){
+    assert!(file_exist("file name"));
+}
+
+#[allow(dead_code)]
+pub fn read_urdf(file: &str) -> bool{
+    if !file_exist(file) {
+        return false;
+    }
+
+    true
+}
+
+#[test]
+fn test_read_urdf(){
+    assert!(read_urdf("file name"));
+}
