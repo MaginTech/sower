b0f6772ea9795ca995a2d93579608835c85df76b
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2022-05-06 15:56:15 +0000

    [add] robot

---
+++ src/robot.rs
@@ -0,0 +1,34 @@
+extern crate nalgebra as na;
+
+use crate::math::*;
+use crate::link::*;
+
+// robot structure.
+#[derive(Clone, PartialEq)]
+pub struct Robot{
+    name: String,
+    id: u32,
+
+    // link
+    root : Option<Box<Link>>,
+
+    // general coordinate
+    gen_coord: na::DVector<f64>,
+    gen_veloc: na::DVector<f64>,
+    gen_accel: na::DVector<f64>,
+    gen_force: na::DVector<f64>,
+}
+
+impl Default for Robot {
+    fn default() -> Self { 
+        Self{
+            name: Default::default(),
+            id: Default::default(),
+            root: Default::default(),
+            gen_coord: na::DVector::from_element(1, 0.),
+            gen_veloc: na::DVector::from_element(1, 0.),
+            gen_accel: na::DVector::from_element(1, 0.),
+            gen_force: na::DVector::from_element(1, 0.),
+        }
+    }
+}
