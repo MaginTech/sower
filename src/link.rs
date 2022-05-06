018115018c20568937eed056d48eb7ba39433e51
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2022-05-06 11:01:59 +0000

    [fix] for nalgebra

--- src/link.rs
+++ src/link.rs
@@ -1,7 +1,9 @@
-extern crate ndarray;
-extern crate ndarray_linalg;
+// extern crate ndarray;
+// extern crate ndarray_linalg;
 
-use ndarray::prelude::*;
+// use ndarray::prelude::*;
+
+extern crate nalgebra as na;
 
 #[derive(Clone, PartialEq)]
 pub enum JointType {
@@ -11,43 +13,44 @@ pub enum JointType {
     Prismatic, 
 }
 
-impl Default for JointType {
-    fn default() -> Self { JointType::Revolute }
-}
-
 #[derive(Clone, PartialEq)]
 pub enum JointMode {
     Active,
     Passive,
 }
 
-impl Default for JointMode {
-    fn default() -> Self { JointMode::Active }
-}
-
 /// joint structure.
-#[derive(Clone, PartialEq, Default)]
+#[derive(Clone, PartialEq)]
 pub struct Joint{
     joint_type: JointType,
     mode: JointMode,
 
     // joint displacement
-    position: Array1<f64>
+    // position: Array1<f64>
+    position: na::DVector<f64>
 }
 
-impl Joint{
-    pub fn position(&self) -> &Array1<f64> {
-        &self.position
+impl Default for Joint {
+    fn default() -> Self { 
+        Self{
+            joint_type: JointType::Revolute,
+            mode: JointMode::Active,
+            position : na::DVector::from_element(1, 0.)
+        }
     }
+}
+
+ impl Joint{
+
 }
 
 #[test]
 fn test_joint_position(){
     let joint = Joint { 
-        position : array![1.5,],
+        position : na::DVector::from_element(1, 1.5),
         ..Default::default() };
-
-    assert_eq!(array![1.5,], joint.position());
+    let ref_value = na::DVector::from_element(1, 1.5);
+    assert_eq!(ref_value, joint.position);
 }
 
 #[derive(Clone, PartialEq)]
@@ -60,16 +63,19 @@ impl Default for LinkType {
 }
 
 /// link structure.
-#[derive(Clone, PartialEq, Default)]
+#[derive(Clone, PartialEq)]
 pub struct Link{
     name: String,
     id: u32,
     link_type: LinkType,
     
     // arm vector of link
-    arm_vec: Array1<f64>,
+    // arm_vec: Array1<f64>,
+    arm_vec: na::Vector3<f64>,
+
     // arm rotation of link
-    arm_rot: Array2<f64>,
+    // arm_rot: Array2<f64>,
+    arm_rot: na::Matrix3<f64>,
 
     joint: Joint,
 
@@ -78,9 +84,12 @@ pub struct Link{
     brother: Option<Box<Link>>,
     
     // position from root link
-    position: Array1<f64>,
+    // position: Array1<f64>,
+    position: na::Vector3<f64>,
+
     // rotation matrix from root link
-    rotation: Array2<f64>,
+    // rotation: Array2<f64>,
+    rotation: na::Matrix3<f64>,
 
     // // twist velocity from root link
     // twist_vel: Array1<f64>,
@@ -88,79 +97,97 @@ pub struct Link{
     // twist_acc: Array1<f64>,
 }
 
+impl Default for Link {
+    fn default() -> Self { 
+        Self{
+            name: Default::default(),
+            id: Default::default(),
+            link_type: LinkType::Rigid,
+            arm_vec: na::Vector3::new(0., 0., 0.),
+            arm_rot: na::Matrix3::new
+                (1., 0., 0.,
+                 0., 1., 0.,
+                 0., 0., 1.),
+            joint: Default::default(),
+            parent: None,
+            child: None,
+            brother: None,
+            position: na::Vector3::new(0., 0., 0.),
+            rotation: na::Matrix3::new
+                (1., 0., 0.,
+                 0., 1., 0.,
+                 0., 0., 1.),
+        }
+    }
+}
+
 impl Link{
-    pub fn position(&self) -> &Array1<f64> {
-        &self.position
+    // fn mut_child(&mut self) -> Option<Box<Link>> {
+    //     self.child.borrow_mut()
+    // }
+
+    fn joint_type(&self) -> &JointType{
+        &self.joint.joint_type
     }
 
-    pub fn rotation(&self) -> &Array2<f64> {
-        &self.rotation
+    pub fn position(&self) -> na::Vector3<f64> {
+        self.position
     }
 
-    pub fn update_link_frame(l: Link){
-    // if child
-    //     match Link.type{
-    //         fix =>  println!("to be implement");,
-    //         free =>  println!("to be implement");,
-    //         revolute => 
-    //             child.position = position + arm_pos;
-    //             let rot = arm_rot * rot_mat(joit.position(0));
-    //             child.rotation = arm_rot * rotation;,
-    //         prismatic => println!("to be implement"),
-    //     }
+    pub fn rotation(&self) -> na::Matrix3<f64> {
+        self.rotation
+    }
+
+    pub fn update_link_framemut(&mut self){
+        let parent = &self.parent;
+         if let Some(p) = parent { 
+            match self.joint_type(){
+                JointType::Fix => println!("to be implement"),
+                JointType::Free => println!("to be implement"),
+                JointType::Revolute => 
+                {
+                    self.position = p.position + p.arm_vec;
+                    // let rot = arm_rot * rot_mat(joit.position);
+                    let rot = na::Matrix3::<f64>::identity();
+                    self.rotation = rot * p.arm_rot *  p.rotation;
+                },
+                JointType::Prismatic => println!("to be implement"),
+            }
+        }
     }
 }
 
 #[test]
 fn test_link_position(){
     let link = Link { 
-        position : array![0., 1., 2.],
+        position : na::Vector3::new(0., 1., 2.),
         ..Default::default() };
 
-    assert_eq!(array![0., 1., 2.], link.position());
+    let ref_value = na::Vector3::new(0., 1., 2.);
+    assert_eq!(ref_value, link.position());
 }
 
 #[test]
 fn test_update_link_frame() {
-    let mut child_joint = Joint {
-        joint_type : JointType::Revolute,
-        mode : JointMode::Active,
-        position : array![5.],
-    };
-
-    let mut child = Link {
+    let parent = Link { 
         name : "1".to_string(),
-        id : 1,
-        link_type : LinkType::Rigid,
-        arm_vec : array![0., 1., 0.],
-        arm_rot : array![[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]],
-        joint : child_joint,
-        parent : None, 
-        child : None, 
-        brother : None, 
-        position : array![0., 1., 0.],
-        rotation : array![[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]],
+        id : 0,
+        position: na::Vector3::new(0., 1., 0.),
+        ..Default::default() 
     };
 
-    let mut link_joint = Joint {
-        joint_type : JointType::Revolute,
-        mode : JointMode::Active,
-        position : array![5.],
+    let mut joint = Joint {
+        position : na::DVector::from_element(1, 1.5),
+        ..Default::default()
     };
 
     let mut link = Link {
         name : "0".to_string(),
-        id : 0,
-        link_type : LinkType::Rigid,
-        arm_vec : array![0., 1., 0.],
-        arm_rot : array![[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]],
-        joint : link_joint,
-        parent : None, 
-        child : Some(Box::new(child)), 
-        brother : None, 
-        position : array![0., 1., 0.],
-        rotation : array![[1., 0., 0.],[0., 1., 0.],[0., 0., 1.]],
+        id : 1,
+        joint : joint,
+        parent : Some(Box::new(parent)), 
+        ..Default::default()
     };
 
-    // assert_eq!(array![0., 1., 0.], link.child);
+    // assert_eq!(array![0., 1., 0.], Rc::strong_count(&link.child.position));
 }
